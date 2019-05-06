93c0c5f0d684feef986d0c8d6377a6584edef65a
Robin Wong <chaowang0313@gmail.com>
2019-05-06 20:25:39 +0800

    [Frontend] just for a try

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,168 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section>
+    <div class="container category">
+      <p class="title">通知公告 - 教务部</p>
+      <div class="tile is-ancestor">
+        <!-- The magical tile element! -->
+        <div class="tile is-parent is-4">
+          <div class="tile is-child card">
+            <header class="card-header">
+              <p class="card-header-title">
+                教务部关于应届本科毕业生申报2019年广州市“菁英计划”留学项目的通知
+              </p>
+            </header>
+            <div class="card-content">
+              <div class="content">
+                各学院、直属系，各附属医院：<br>
+                根据《广州市“菁英计划”留学项目领导小组办公室关于申报2019年广州市“菁英计划”留学项目的通知》（穗菁〔201９〕1号）(附件1)文件精神，现将相关事项通知如下：...
+              </div>
+              <time datetime="2019-05-06">发布日期：2019/05/06</time>
+            </div>
+            <footer class="card-footer">
+              <a href="#" class="card-footer-item">全文</a>
+              <a href="#" class="card-footer-item">原文链接</a>
+              <a href="#" class="card-footer-item">分享</a>
+            </footer>
+          </div>
+        </div>
+
+        <div class="tile is-parent is-4">
+          <div class="tile is-child card">
+            <header class="card-header">
+              <p class="card-header-title">
+                教务部关于开展2019年中山大学实验室开放基金项目推荐工作的通知
+              </p>
+            </header>
+            <div class="card-content">
+              <div class="content">
+                各学院、直属系：<br>
+                按照学校“十二字”人才培养目标要求，为促进我校学生对所学理论知识的拓展，强化科研意识、创新精神和实践能力的培养，鼓励学生开展创新训练和实践，学校拟在各院系开展2019年实验室开放基金项目遴选推荐工作。现将有关事项通知如下：...
+              </div>
+              <time datetime="2019-05-06">发布日期：2019/05/06</time>
+            </div>
+            <footer class="card-footer">
+              <a href="#" class="card-footer-item">全文</a>
+              <a href="#" class="card-footer-item">原文链接</a>
+              <a href="#" class="card-footer-item">分享</a>
+            </footer>
+          </div>
+        </div>
+
+        <div class="tile is-parent is-4">
+          <div class="tile is-child card">
+            <header class="card-header">
+              <p class="card-header-title">
+                教务部关于组织对广州市高校创新创业教育项目进行结项验收的通知
+              </p>
+            </header>
+            <div class="card-content">
+              <div class="content">
+                各有关单位：<br>
+                根据《广州市教育局关于组织对广州市高校创新创业教育项目及部分延期或验收不通过项目进行结项验收的通知》（穗教高教〔2019〕11号，附件1）要求, 广州市教育局拟于近期组织专家对上述相关立项项目进行结项验收工作。现将有关事项通知如下：...
+              </div>
+              <time datetime="2019-05-06">发布日期：2019/05/06</time>
+            </div>
+            <footer class="card-footer">
+              <a href="#" class="card-footer-item">全文</a>
+              <a href="#" class="card-footer-item">原文链接</a>
+              <a href="#" class="card-footer-item">分享</a>
+            </footer>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+      <div class="container category">
+      <p class="title">本科生教务 - SDCS</p>
+      <div class="tile is-ancestor">
+        <!-- The magical tile element! -->
+        <div class="tile is-parent is-4">
+          <div class="tile is-child card">
+            <header class="card-header">
+              <p class="card-header-title">
+                转：教务部关于开展2019年中山大学实验室开放基金项目推荐工作的通知
+              </p>
+            </header>
+            <div class="card-content">
+              <div class="content">
+                 各位同学：<br>
+                 现转发《教务部关于开展2019年中山大学实验室开放基金项目推荐工作的通知》（附件1），有意向申报的学生团队，请于5月13日中午12点前提交《中山大学实验室开放基金项目申请书》（附件2）纸质版一式三份至学院A111吴老师处，电子版《中山大学实验室开放基金项目申请书》及《中山大学实验室开放基金项目信息登记汇总表》（附件3）请发至吴老师邮箱：wulanlan@mail.sysu.edu.cn。...
+              </div>
+              <time datetime="2019-05-06">发布日期：2019/05/06</time>
+            </div>
+            <footer class="card-footer">
+              <a href="#" class="card-footer-item">全文</a>
+              <a href="#" class="card-footer-item">原文链接</a>
+              <a href="#" class="card-footer-item">分享</a>
+            </footer>
+          </div>
+        </div>
+
+        <div class="tile is-parent is-4">
+          <div class="tile is-child card">
+            <header class="card-header">
+              <p class="card-header-title">
+                数据科学与计算机学院2019年拟接收校内转专业学生名单公示
+              </p>
+            </header>
+            <div class="card-content">
+              <div class="content">
+                根据教务部关于加强和规范2019年本科生转专业工作的补充通知（教务〔2019〕72号），现对经我院党政联席会议审议通过的我院2019年本科生转专业拟接收学生名单进行公示（详见附件），公示日期为2019年4月22日至4月26日。...
+              </div>
+              <time datetime="2019-04-22">发布日期：2019/04/22</time>
+            </div>
+            <footer class="card-footer">
+              <a href="#" class="card-footer-item">全文</a>
+              <a href="#" class="card-footer-item">原文链接</a>
+              <a href="#" class="card-footer-item">分享</a>
+            </footer>
+          </div>
+        </div>
+
+        <div class="tile is-parent is-4">
+          <div class="tile is-child card">
+            <header class="card-header">
+              <p class="card-header-title">
+                2019年中山大学大学生创新训练计划项目推荐立项公示
+              </p>
+            </header>
+            <div class="card-content">
+              <div class="content">
+                根据《教务部关于关于开展2019年中山大学大学生创新训练计划项目推荐工作的通知》的文件要求，2019年我院收到创新训练项目申请8项。本年度学校下达给我院的限项项数为3项（国家级1项，省级1项，校级1项）。...
+              </div>
+              <time datetime="2019-04-19">发布日期：2019/04/19</time>
+            </div>
+            <footer class="card-footer">
+              <a href="#" class="card-footer-item">全文</a>
+              <a href="#" class="card-footer-item">原文链接</a>
+              <a href="#" class="card-footer-item">分享</a>
+            </footer>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+// import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
+    // HelloWorld,
   },
 };
 </script>
+
+<style>
+  .tile {
+    align-items: start;
+  }
+  .category {
+    margin-bottom: 4rem;
+  }
+</style>
