c6f305d044521ad77c6e3b36e8bdd8e1d8409644
fffengmjl <m781131011@outlook.com>
2019-05-18 23:20:52 +0800

    debug

--- client.py
+++ client.py
@@ -7,11 +7,7 @@ from flask import request
 from flask import jsonify
 import sqlite3
 
-# template = {'ret', 'data'}
-ERROR_LIST = ['SUBSCRIPTION_ID_NOT_FOUND']
-
 client = Flask(__name__)
-
 
 '''
 用户订阅的所有 source 的数组，及 source 对应的部分 news。
@@ -35,7 +31,6 @@ def home_test():
 
             for row_new in news:
                 news_list.append({'id' : row_new[0], 'title' : row_new[1], 'publishDate' : row_new[2], 'fetchTime' : row_new[3], 'is_favorite' : row_new[4], 'keywords' : row_new[5].split('，'), 'excerpt' : row_new[6], 'url' : row_new[7]})
-            print(jsonify(news_list[0]))
             src_list.append({'id' : row_src[0], 'name' : row_src[1], 'department' : row_src[2], 'news' : news_list})
         
         c_source.close()
@@ -82,7 +77,6 @@ def source_tar(id):
 
             for row_new in news:
                 news_list.append({'id' : row_new[0], 'title' : row_new[1], 'publishDate' : row_new[2], 'fetchTime' : row_new[3], 'is_favorite' : row_new[4], 'keywords' : row_new[5].split('，'), 'excerpt' : row_new[6], 'url' : row_new[7]})
-            print(jsonify(news_list[0]))
             src_list.append({'id' : row_src[0], 'name' : row_src[1], 'department' : row_src[2], 'news' : news_list})
     
         c_src.close()
@@ -119,55 +113,63 @@ def give_news(id):
 @client.route('/subscription/',  methods = ['GET'])
 def subscription():
     if request.method == 'GET' :
-        fav_page = request.args.get('page', 0, type = int)
-        fav_num = request.args.get('limit', 1, type = int)
+        fav_page = request.args.get('page', 1, type = int)
+        fav_num = request.args.get('limit', 0, type = int)
         s_limit = ''
-        if fav_num :
+        if fav_num != 0 :
             s_limit = 'LIMIT %d OFFSET %d' % (fav_num, (fav_page - 1) * fav_num)
-        data = sqlite3.connect('test.db')
-        c = data.cursor()
 
-        sub = c.execute('SELECT source_id FROM SUBSCRIPTION ORDER BY source_id ASC %s;' % s_limit)
+        data = sqlite3.connect('test.db')
+        c_src = data.cursor()
+
+        sub = c_src.execute('SELECT source_id FROM SUBSCRIPTION ORDER BY source_id ASC %s;' % s_limit)
         sub_list = []
+        src_id_ls = []
+        
         for row_sub in sub:
-            sources = c.execute('SELECT source_id, source_sub_name, source_department_name FROM SOURCE WHERE source_id = %d;' % row_sub[0])
+            src_id_ls.append(row_sub[0])
+        for row_id in src_id_ls :
+            sources = c_src.execute('SELECT source_id, source_sub_name, source_department_name FROM SOURCE WHERE source_id = %d;' % row_id)
+            for row_src in sources :
+                sub_list.append({'id' : row_src[0], 'name' : row_src[1], 'department' : row_src[2]})
+        '''
+        # 我也不知道为什么这个不行
+        for row_sub in sub:
+            sources = c_src.execute('SELECT source_id, source_sub_name, source_department_name FROM SOURCE WHERE source_id = %d;' % row_sub[0])
             for row_src in sources:
                 sub_list.append({'id' : row_src[0], 'name' : row_src[1], 'department' : row_src[2]})
-
-        c.close()
+        '''
+        c_src.close()
         data.close()
         return jsonify({'ret' : 0, 'data' : sub_list})
     else :
         return jsonify({'ret' : 1, 'data' : 'NULL'})
 
-@client.route('/subcription/<int:id>', methods = ['POST'])
-def add_sub(id):
+@client.route('/subscription/<int:src_id>', methods = ['POST'])
+def add_sub(src_id):
     if request.method == 'POST':
         data = sqlite3.connect('test.db')
         c = data.cursor()
         passport = 0
 
-        sources = c.execute('SELECT source_id FROM SOURCE WHERE source_id = %d;' % id)
+        sources = c.execute('SELECT source_id FROM SOURCE WHERE source_id = %d;' % src_id)
         for row_src in sources :
-            if row_src[0] == id:
+            if row_src[0] == src_id:
                 passport = 1
         if passport == 0 :
             return jsonify({'ret' : 243, 'data' : 'SOURCE_ID_NOT_FOUND'})
         
-        passport = 0
-        sub = c.execute('SELECT source_id FROM SUBSCRIPTION WHERE source_id = %d;' % id)
+        sub = c.execute('SELECT source_id FROM SUBSCRIPTION WHERE source_id = %d;' % src_id)
         for row_sub in sub:
-            if row_sub[0] == id:
-                passport = 1
-        if passport == 1 :
+            if row_sub[0] == src_id:
+                passport = 0
+        if passport == 0 :
             return jsonify({'ret' : 242, 'data' : 'IS_ALREADY_SUBSCRIBED'})
 
-        c.execute('INSERT INTO SUBSCRIPTION (source_id, last_update_time) VALUES (%d, %s)' % (id, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
-        sub_ids = c.execute('SELECT sub_id FROM SUBSCRIPTION WHERE source_id = %d' % id)
-        sub_id = 0
-        for row_id in sub_id :
-            sub_id  = row_id[0]
-        c.execute('UPDATE SOUCRCE SET subscription_id = %d WHERE source_id = %d' % (sub_id, id))
+        c.execute('INSERT INTO SUBSCRIPTION (source_id, last_update_time) VALUES (%d, "%s");' % (src_id, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+        test = c.execute('SELECT * FROM SUBSCRIPTION WHERE source_id = %d;' % src_id)
+        for row in test :
+            print(row)
         data.commit()
         c.close()
         data.close()
@@ -176,7 +178,7 @@ def add_sub(id):
     else :
         return jsonify({'ret' : 1, 'data' : 'NULL'})
 
-@client.route('/subcription/<int:id>', methods = ['DELETE'])
+@client.route('/subscription/<int:id>', methods = ['DELETE'])
 def del_sub(id):
     if request.method == 'DELETE':
         data = sqlite3.connect('test.db')
@@ -199,7 +201,6 @@ def del_sub(id):
             return jsonify({'ret' : 242, 'data' : 'IS_ALREADY_NOT_SUBSCRIBED'})
 
         c.execute('DELETE FROM SUBSCRIPTION WHERE source_id = %d;' % id)
-        c.execute('UPDATE SOUCRCE SET subscription_id = 0 WHERE subscription_id = %d;' % id)
         data.commit()
         c.close()
         data.close()
@@ -236,16 +237,16 @@ def fav_get():
 def add_fav(id):
     if request.method == 'POST':
         passport = 0
-        data = sqlite3.connect('tast.db')
+        data = sqlite3.connect('test.db')
         c = data.cursor()
-        news_sub = c.execute('SELECT news_id is_bookmarked FROM NEWS WHERE news_id = %d;' % id)
+        news_sub = c.execute('SELECT news_id, is_bookmarked FROM NEWS WHERE news_id = %d;' % id)
         for row_news_sub in news_sub :
             if row_news_sub[0] == id :
                 passport = 1
                 if row_news_sub[1] == 1:
                     passport = 2
         if passport == 1 :
-            c.execute('UPDATE NEWS SET is_bookmarked = 1 WHERR news_id = %d;' % id)
+            c.execute('UPDATE NEWS SET is_bookmarked = 1 WHERE news_id = %d;' % id)
             c.close()
             data.commit()
             data.close()
@@ -265,16 +266,16 @@ def add_fav(id):
 def del_fav(id):
     if request.method == 'DELETE':
         passport = 0
-        data = sqlite3.connect('tast.db')
+        data = sqlite3.connect('test.db')
         c = data.cursor()
-        news_sub = c.execute('SELECT news_id, id_bookmarked FROM NEWS WHERE news_id = %d;' % id)
+        news_sub = c.execute('SELECT news_id, is_bookmarked FROM NEWS WHERE news_id = %d;' % id)
         for row_news_sub in news_sub :
             if row_news_sub[0] == id :
                 passport = 1
                 if row_news_sub[1] == 1:
                     passport = 2
         if passport == 1 :
-            c.execute('UPDATE NEWS SET is_bookmarked = 0 WHERR news_id = %d;' % id)
+            c.execute('UPDATE NEWS SET is_bookmarked = 0 WHERE news_id = %d;' % id)
             c.close()
             data.commit()
             data.close()
@@ -283,7 +284,7 @@ def del_fav(id):
             c.close()
             data.close()
             return jsonify({'ret' : 233, 'data' : 'NEWS_ID_NOT_FOUND'})
-        elif passport == 1 :
+        elif passport == 2 :
             c.close()
             data.close()
             return jsonify({'ret' : 223, 'data' : 'IS_ALREADY_NOT_FAVORITE'})
