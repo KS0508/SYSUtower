730ab368dac315302cd8a8d94cc9e1cb581aa3f8
Robin Wong <chaowang0313@gmail.com>
2019-05-22 00:13:22 +0800

    [BuildingTest] add untracked files

---
+++ frontend/vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        // options placed here will be merged with default configuration and passed to electron-builder
+        productName: 'SYSUTower',
+        win: {
+          target: 'portable',
+        }
+      }
+    }
+  }
+}
